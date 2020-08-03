94d778734ce2332bb24d6287b78141787f1c4ebe
Thaoltp88 <thaolp2611@gmail.com>
2020-08-03 15:52:04 +0700

    add interaction

--- app/src/main/java/com/example/androidcourse/MainActivity.kt
+++ app/src/main/java/com/example/androidcourse/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.androidcourse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun clickHandler(view: View){
+        var editText = findViewById<EditText>(R.id.nick_edit)
+        var nameText = findViewById<TextView>(R.id.nick_text)
+        nameText.visibility = View.VISIBLE
+        nameText.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
     }
 }
